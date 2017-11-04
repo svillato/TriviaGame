f7c5afefe1f0ffb2c8570fb66571a53c0bf3a08e
Susan Villatoro <susanvillatoro@Susans-MBP.attlocal.net>
2017-11-03 22:43:05 -0700

    functional game

---
+++ assets/javascript/app.js
@@ -0,0 +1,149 @@
+var panel = $('#quiz-area');
+var countStartNumber = 30;
+
+
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
+
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  game.loadQuestion();
+});
+
+
+var questions = [{
+  question: "What Disney Park has a dragon under its castle?",
+  answers: ["Magic Kindom", "Disneyland Paris", "Disneyland Tokyo"],
+  correctAnswer: "Disneyland Paris",
+}, {
+  question: "What Disney character is hidden in multiple areas of the Disney Parks?",
+  answers: ["Aladdin", "Goofy", "Mickey Mouse"],
+  correctAnswer: "Mickey Mouse",
+}, {
+  question: "Which Disney movie was the first to have been made into a Broadway musical?",
+  answers: ["Beauty and the Beast", "The Lion King", "Mary Poppins"],
+  correctAnswer: "Beauty and the Beast",
+}, {
+  question: 'Which Disney character sang, "Hellfire"?',
+  answers: ["Claude Frollo", "Hades", "Amos Slade"],
+  correctAnswer: "Claude Frollo",
+}, {
+  question: 'Which popular Disney movie featured the song, "supercalifragilisticexpialidocious"?',
+  answers: ["Pete's Dragon", "Hercules", "Mary Poppins"],
+  correctAnswer: "Mary Poppins",
+}, {
+  question: "What is the name of the street when you walk down to get to the castle at the Disney Parks?",
+  answers: ["Mickey's Street", "Main Street", "Disney Street"],
+  correctAnswer: "Main Street",
+}, {
+  question: "In which Disney World Park can you travel around the world?",
+  answers: ["Epcot", "Animal Kingdom", "Magic Kingdom"],
+  correctAnswer: "Epcot",
+}, {
+  question: "Which movie was based on the events leading to the making of Marry Poppins?",
+  answers: ["Saving Mr. Banks", "The Making of Poppins", "Mr. Disney"],
+  correctAnswer: "Saving Mr.Banks"
+}];
+
+
+
+
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:countStartNumber,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.timeUp();
+    }
+  },
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
+    panel.html('<h2>Out of Time!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    panel.html('<h2>All done, heres how you did!</h2>');
+    $('#counter-number').html(game.counter);
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+    panel.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    game.incorrect++;
+    clearInterval(timer);
+    panel.html('<h2>Nope!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    panel.html('<h2>Correct!</h2>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
